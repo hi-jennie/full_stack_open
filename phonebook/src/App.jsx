47e2bd2ac51a96bd212f2809b0795636bd2cff7f
hi-jennie <jennie.wang2024@gmail.com>
2025-06-14 23:42:58 +0800

    finished the Phonebook step 1

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,1 +1,43 @@
+import { useState } from "react";
 
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const newPerson = {
+      name: newName,
+    };
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(newPerson));
+      setNewName("");
+    }
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ol>
+        {persons.map((person, index) => (
+          <li key={index}>{person.name}</li>
+        ))}
+      </ol>
+      <div>debug: {newName}</div>
+    </div>
+  );
+};
+
+export default App;
