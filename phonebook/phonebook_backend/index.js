d8a1d2dc749acea3651933b2f8e6c1617960655e
hi-jennie <jennie.wang2024@gmail.com>
2025-08-13 00:04:20 +0800

    refactor: Connecting the backend to a database

--- phonebook/phonebook_backend/index.js
+++ phonebook/phonebook_backend/index.js
@@ -7,6 +7,34 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+const password = process.argv[2]
+
+const url = `mongodb+srv://jenniewang2024:${password}@cluster0.4bjnssi.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+const Person = mongoose.model('person', personSchema)
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        // 把 _id 改成 id（字符串）
+        returnedObject.id = returnedObject._id.toString()
+        // 删除 _id 和 __v
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
 
 // define a token called body, the return value of the callback function will be assigned to the body token
 morgan.token('body', (request) => {
@@ -15,66 +43,46 @@ morgan.token('body', (request) => {
 // the  parameter in morgan defines the format of the log
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
 
 // get all persons
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(people => {
+        response.json(people)
+    })
 })
 
-//  get single person through id
-app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const targetPerson = persons.find(p => Number(p.id) === id)
-    if (targetPerson) {
-        response.json(targetPerson);
-    } else {
-        response.status(404).end();
-    }
-})
+// //  get single person through id
+// app.get('/api/persons/:id', (request, response) => {
+//     const id = Number(request.params.id);
+//     const targetPerson = persons.find(p => Number(p.id) === id)
+//     if (targetPerson) {
+//         response.json(targetPerson);
+//     } else {
+//         response.status(404).end();
+//     }
+// })
 
-// delete person through id
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id;
-    const deletedPerson = persons.find(p => p.id === id)
-    if (deletedPerson) {
-        persons = persons.filter(p => p.id !== id)
-        response.status(204).end()
-    } else {
-        response.status(404).end()
-    }
-})
+// // delete person through id
+// app.delete('/api/persons/:id', (request, response) => {
+//     const id = request.params.id;
+//     const deletedPerson = persons.find(p => p.id === id)
+//     if (deletedPerson) {
+//         persons = persons.filter(p => p.id !== id)
+//         response.status(204).end()
+//     } else {
+//         response.status(404).end()
+//     }
+// })
 
-app.get('/api/info', (request, response) => {
-    const time = new Date();
-    response.send(`Phonebook has info for ${persons.length} people<br>${time}`)
-})
+// app.get('/api/info', (request, response) => {
+//     const time = new Date();
+//     response.send(`Phonebook has info for ${persons.length} people<br>${time}`)
+// })
 
-const generatedId = () => {
-    const maxId = persons.length > 0 ? Math.max(...persons.map(p => Number(p.id))) : 0
-    return String(maxId + 1);
-}
+// const generatedId = () => {
+//     const maxId = persons.length > 0 ? Math.max(...persons.map(p => Number(p.id))) : 0
+//     return String(maxId + 1);
+// }
 app.post('/api/persons', (request, response) => {
     const body = request.body
     if (!body.name) {
@@ -84,24 +92,40 @@ app.post('/api/persons', (request, response) => {
     if (!body.number) {
         return response.status(400).json({ error: 'number is missing' })
     }
-    const existingPerson = persons.find(p => p.name.toUpperCase() === body.name.toUpperCase())
-    if (existingPerson) {
-        return response.status(400).json({
-            error: 'name already existing'
+    Person.find({}).then(people => {
+        const existingPerson = people.find(p => p.name.toUpperCase() === body.name.toUpperCase())
+        if (existingPerson) {
+            return response.status(400).json({
+                error: 'name already existing'
+            })
+        }
+
+        const newPerson = new Person({
+            name: body.name,
+            number: body.number
         })
-    }
-
-    const newPerson = {
-        id: generatedId(),
-        name: body.name,
-        number: body.number
-    }
-    persons = persons.concat(newPerson);
-    response.json(newPerson)
+        return newPerson.save()// 这里加return，继续链式调用
+    }).then(result => {
+        if (result) {
+            console.log(`added ${result.name} number ${result.number} to phonebook`)
+            response.json(result)  // 响应客户端
+            // 注意：不要在这里关闭连接,不是每次请求完成后都要关数据库
+        }
+    }).catch(error => {
+        console.error(error)
+        response.status(500).json({ error: 'server error' })
+    })
 })
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
 
-})
+mongoose.connect(url)
+    .then(() => {
+        const PORT = process.env.PORT || 3001
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        })
+    })
+    .catch(err => {
+        console.error('error connecting to MongoDB:', err.message)
+    })
+
