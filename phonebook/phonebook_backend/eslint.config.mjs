e5718118fdb6b0b230c985a36a62926236efac23
hi-jennie <jennie.wang2024@gmail.com>
2025-09-22 15:49:03 +0200

    fix: update ESLint configuration to enforce additional rules and remove unnecessary file specification

--- phonebook/phonebook_backend/eslint.config.mjs
+++ phonebook/phonebook_backend/eslint.config.mjs
@@ -1,6 +1,5 @@
 import globals from 'globals'
 import js from '@eslint/js'
-
 import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
@@ -12,9 +11,6 @@ export default [
       globals: { ...globals.node },
       ecmaVersion: 'latest',
     },
-  },
-  {
-    files: ['**/*.js'],
     plugins: {
       '@stylistic/js': stylisticJs,
     },
@@ -23,9 +19,14 @@ export default [
       '@stylistic/js/linebreak-style': ['error', 'unix'],
       '@stylistic/js/quotes': ['error', 'single'],
       '@stylistic/js/semi': ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 'off',
     },
   },
   {
     ignores: ['dist/**'],
-  }
+  },
 ]
